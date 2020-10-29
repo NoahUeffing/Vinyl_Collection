10869db543d6bf65e2ed3ab838f74f06bce230e9
Noah Ueffing <noahueffing@gmail.com>
2020-10-29 14:52:13 -0300

    Started front end

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className="my-1">
+        This is a full stack React app for keeping music collections. Created by
+        Noah Ueffing for Acadia COMP 3753: Database Management Systems.
+      </p>
+      <p className="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
